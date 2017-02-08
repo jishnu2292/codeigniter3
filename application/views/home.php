7e53eca2187dab290cf39012b4f2c04b1c69e538
Jishnu <jishnu2292@gmail.com>
2017-02-08 12:02:43 +0530

    codeigniter set up with removing index.php from url

---
+++ application/views/home.php
@@ -0,0 +1,17 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?><!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>Welcome to CodeIgniter</title>
+
+</head>
+<body>
+
+<a href="home/test">Test</a>
+<br>
+<a href="<?php echo base_url() ?>home/test">Test</a>
+
+</body>
+</html>
